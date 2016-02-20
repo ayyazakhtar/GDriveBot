d48b1389501e78e1dd0b35e50b52ac49883615c9
ayyazakhtar <aze591@gmail.com>
2016-02-20 23:26:30 +0500

    Version of TV shows without the sqlite database

--- kat_search.py
+++ kat_search.py
@@ -11,109 +11,66 @@ import json
 
 
 url_base= 'https://www.kat.cr/'
-x = "https://kat.cr/usearch/DC'sLegends of tomorrow S01E01 category%3Atv/?field=seeders&sorder=desc"
-# url_temp= "https://kat.cr/usearch/DC's legends of tomorrow/"
+# x = "https://kat.cr/usearch/DC'sLegends of tomorrow S01E01 category%3Atv/?field=seeders&sorder=desc"s
 
 def get_tv_show_episode(show_name, season, episode):
 	if(season < 10):
-		ep_string = "S0" + str(season)	;
+		ep_string = "S0" + str(season)
 	else:
-		ep_string =  "S" + str(season)	;
+		ep_string =  "S" + str(season)
 
 	if(episode < 10):	
-		ep_string = ep_string  + "E0" + str(episode)	;
+		ep_string = ep_string  + "E0" + str(episode)
 	else:
-		ep_string = ep_string  + "E" + str(episode)	;		
+		ep_string = ep_string  + "E" + str(episode)		
 
-	search_string = show_name + " " + ep_string
-	torrent = get_magnet_link(search_string, "tv")
+	torrent = get_magnet_link([show_name, ep_string], "tv")
 	
+	if torrent == None:
+		print("Episdoe not found")
+		return None;
+	else:
+		return torrent
+	# print "name : " + torrent['name']
+	# print "size : " + torrent['size']
+	# print "seeders: " + torrent['seeders']
+	# print  "magnet Link : " + torrent['magnet']
+
+def search_keyword_in_name(name, keywords):
+	match = True
+	name = name.lower()		
+	name = name.split(' ')
+	
+	for keyword in keywords:		
+		search_res = [s for s in name if keyword.lower() == s]
+		if search_res == []:
+			match = False
+			
+	return match
 
 
-	if (((torrent['name'].lower()).find(show_name.lower()) == -1 ) or ((torrent['name'].lower()).find(ep_string.lower()) == -1) ):
-		print "episode not found"
-	else :
-		print "name : " + torrent['name']
-		print "size : " + torrent['size']
-		print "seeders: " + torrent['seeders']
-		print  "magnet Link : " + torrent['magnet']
-
-
-
-
-
-def get_magnet_link(torrent_name, category):
-	print "----------------------------------------------------------------"
-	url = url_base + "usearch/" + torrent_name + " category%3A" + category +  "/?field=seeders&sorder=desc"
-	print url
+def get_magnet_link(search_list, category):
+	# print "----------------------------------------------------------------"
+	url = url_base + "usearch/" + ' '.join(search_list) + " category%3A" + category +  "/?field=seeders&sorder=desc"
 	response = requests.get(url, verify= True)
 	soup = BeautifulSoup(response.text,"html.parser")
 	table = soup.find("table", {"class": "data"})
 	torrent = {"name":"", "magnet":"", "size":0, "seeders":0}
 	if table == None:
 		print("page search error")
-		print "----------------------------------------------------------------"
-		return torrent
+		return None
 	
 	for row in table.findAll('tr')[1:]:
-		cur_struct = row.findAll('td')
+		cur_struct = row.findAll('td')			
+		name = cur_struct[0].find(attrs={"class" : "cellMainLink"}).getText()		
+		matched = search_keyword_in_name(name, search_list)
 		
-		torrent['name'] = cur_struct[0].find(attrs={"class" : "cellMainLink"}).getText()
-		torrent['magnet'] = cur_struct[0].find(attrs={"title" : "Torrent magnet link"})['href']
-		torrent['size'] = cur_struct[1].getText()
-		torrent['seeders']= cur_struct[4].getText()	
-		print "----------------------------------------------------------------"
-		return torrent
-
-
-
-
-# for season in range(1,3):
-# 	for episode in range(1,5):
-# 		get_tv_show_episode("DC's Legends of tomorrow", season, episode);
-
-
-
-########################################## ------------ OFFCLoud -------------############################################
-# def get_auth_offcloud():
-# 	url="https://offcloud.com/api/login/classic"
-# 	data = {"username": 'mraqkhan@icloud.com', "password": 'Pakland990'}
-# 	print requests.post(url, data=data).text
-
-# def get_remote_account_list():
-# 	url = 'https://offcloud.com/api/remote-account/list'
-# 	data = {}
-# 	print requests.post(url, data=data).text
-
-
-# session = requests.Session()
-# url="https://offcloud.com/api/login/classic"
-# data = {"username": 'mraqkhan@icloud.com', "password": 'Pakland990'}
-# print session.get(url, data=data).text
-# # url = 'https://offcloud.com/api/login/check'
-
-# url = 'https://offcloud.com/api/remote-account/list'	
-# data = {}
-# account_info =  json.loads(requests.get(url,data=data,cookies=session.cookies.get_dict()).text)
-
-#     # {
-#     #   "accountId": "5686edea098580b372000527",
-#     #   "remoteOptionId": "5686edea098580b372000527",
-#     #   "type": "gdrive",
-#     #   "username": "14mseeakhan@seecs.edu.pk"
-#     # },
-
-# # url = 'https://offcloud.com/api/remote/status/'
-# url = 'https://offcloud.com/api/remote/download'
-# # for account in account_info["data"]:
-# if True:
-# 	print "-------------------------------------------------"
-# 	# print account['username']
-# 	data = {"url" :"magnet:?xt=urn:btih:4B51C4107B41178F60BA6F6411C6A85CA6BA38DC&dn=magic+mike+xxl+2015+1080p+brrip+x264+yify&tr=udp%3A%2F%2Ftracker.publicbt.com%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce" ,"remoteOptionId": '568906c082747ad611000091'}
-# 			# 568906c082747ad611000091
-# 			# ,"requestId": account['remoteOptionId']}
-# 	print requests.post(url,data=data,cookies=session.cookies.get_dict()).text
-
-
-# # get_auth_offcloud();
-# # get_remote_account_list();
+		if matched:
+			torrent['name'] = name
+			torrent['magnet'] = cur_struct[0].find(attrs={"title" : "Torrent magnet link"})['href']
+			torrent['size'] = cur_struct[1].getText()
+			torrent['seeders']= cur_struct[4].getText()				
+			return torrent
+			break
+					
+	return None;
--- search_tv_shows.py
+++ search_tv_shows.py
@@ -1,19 +1,95 @@
 from pytvdbapi import api
+import sys
+import json
+import os
+import fnmatch
+import offcloud_api
+import kat_search
+import sqlite3 as lite
+from datetime import date
+from datetime import datetime
 
+db_name = "torrent_db"
+def get_new_show_input():
+	while True:
+		print("--------------------------------------")
+		name = raw_input("Enter TV Show name?  :  ")
+		results = db.search(name, "en")
+		print "Search Results :"
+		count = 1
+		for result in results:
+			print( str(count)+ " - " + result.SeriesName)
+			count = count + 1
+		print (str(count) + " - Search Again")
+		sel = raw_input("Select 1 - " + str(count) + " : ")
+		if (int(sel) == count):
+			continue
+		else :
+			show = results[int(sel)- 1]
+		print("Selected Series : " + show.SeriesName)
+		show_data = {'title':show.SeriesName, 'imdb_id':show.IMDB_ID, 'cur_season': 1, 'cur_episode':0}
+		show.update()
+		json.dump(show_data, open("tv_show_" + show.SeriesName.replace(' ', '_')+ ".json", 'w'))
+		break
+		
 db = api.TVDB("B43FF87DE395DF56")
-#results = db.search("the flash 2014", "en")
-#for result in results:
-#    print(result.SeriesName)
 
-show = db.get_series( 1796960, "en" , id_type='imdb', cache=True)  # Load Dexter
-print(show.SeriesName)
+if len(sys.argv) > 1:
+	if sys.argv[1] == 'add':
+		get_new_show_input()
 
-#show = results[0];
-#tt2193021
-count = 0
-for season in show:
-    if season.season_number == 0:
-        continue;
-    print '--------------- season ' + str(season.season_number) + ' --------------------'
-    for episode in season:
-        print(u"{0} - {1}".format(episode.EpisodeName, episode.FirstAired))  
+		
+
+# con = lite.connect(db_name)
+
+	
+# with con:    
+	# con.row_factory = lite.Row
+	# cur = con.cursor()
+	
+	# cur.execute("CREATE TABLE IF NOT EXISTS tvshows( imdb_id TEXT, show_title TEXT, season INT, episode INT, episode_title TEXT, status TEXT, offcloud_ref_no INT, magnet_link Text)")
+	
+	# cur.execute("CREATE TABLE IF NOT EXISTS movies( imdb_id TEXT, movie_title TEXT, status TEXT, offcloud_ref_no INT)")
+
+	# # cur.execute("INSERT INTO Cars VALUES(2,'Audi',52642)")
+	# # cur.execute("INSERT INTO Cars VALUES(1,'Mercedes',57127)")
+	
+	# # cur.execute("SELECT * FROM Cars")
+	# # cur.execute("SELECT * FROM Cars WHERE Id=? ", ("1")) 
+	# # rows = cur.fetchall()
+
+	# # for row in rows:
+		# # print row
+
+# exit()
+
+file_list = []
+
+for file in os.listdir('.'):
+	if fnmatch.fnmatch(file, 'tv_show_*.json'):
+		file_list.append(file)
+		
+
+for show_file in file_list:
+	show_data = json.load(open(show_file))	
+	show = db.get_series( show_data["imdb_id"], "en" , id_type='imdb', cache=True)
+	show.update()
+	print '-------------------' + str(show.SeriesName) + ' ------------------------'
+	for season in show:
+		if season.season_number == 0:
+			continue
+		print '--------------- Season' + str(season.season_number) + ' --------------------'
+		for episode in season:	
+			today = (date.today()).toordinal()
+			ep_date = (episode.FirstAired).toordinal()
+			if ep_date < today + 1 : # to download episodes that are atleast a day old.
+				print '----------- Episode ' + str(episode.EpisodeNumber) + ' ----------------'
+				print(u"{0} - {1}".format(episode.EpisodeName, episode.FirstAired)) 
+				#episode_torrent = kat_search.get_tv_show_episode(show.SeriesName, season.season_number, episode.EpisodeNumber)
+				#print episode_torrent['name']
+		
+	exit()
+
+exit()
+
+ 
